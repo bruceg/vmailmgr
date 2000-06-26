d88b2392bb38f94814488a9c2b703d3e2dbafe04
Bruce Guenter <bruce@untroubled.org>
2000-06-26 03:24:20 +0000

    Added usage instructions and a proper test.

--- scripts/autoresponder.sh
+++ scripts/autoresponder.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-test -s $MAILDIR/autoresponse/message.txt && \
-  autorespond 10000 5 $MAILDIR/autoresponse/message.txt $MAILDIR/autoresponse
+#
+# This script checks for the presence of an autoresponse message, and if
+# present executes the autoresponder.
+#
+# To use this autoresponder, copy this file to
+# vmailmgr/vdeliver-postdeliver if it does not already exist.  If it
+# exists, copy this file somewhere accessable and add a call to it to
+# vmailmgr/vdeliver-postdeliver.
+#
+# The autoresponder used here can be found at:
+#	http://www.netmeridian.com/e-huss/autorespond.tar.gz
+#
+
+# amount of time to consider a message (in seconds)
+SENDER_TIME=10000
+# maximum number of messages to allow within SENDER_TIME seconds
+SENDER_NUM=5
+
+if test -s $MAILDIR/autoresponse/message.txt
+then
+  autorespond $SENDER_TIME $SENDER_NUM \
+  	$MAILDIR/autoresponse/message.txt $MAILDIR/autoresponse
+fi

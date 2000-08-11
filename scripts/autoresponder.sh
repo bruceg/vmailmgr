118b29d422cca0b1cd32b2b97c5d65cc72b151f5
Bruce Guenter <bruce@untroubled.org>
2000-08-11 01:09:32 +0000

    Modified to use my own autoresponder.

--- scripts/autoresponder.sh
+++ scripts/autoresponder.sh
@@ -9,16 +9,10 @@
 # vmailmgr/vdeliver-postdeliver.
 #
 # The autoresponder used here can be found at:
-#	http://www.netmeridian.com/e-huss/autorespond.tar.gz
+#	http://em.ca/~bruceg/qmail-autoresponder/
 #
-
-# amount of time to consider a message (in seconds)
-SENDER_TIME=10000
-# maximum number of messages to allow within SENDER_TIME seconds
-SENDER_NUM=5
 
 if test -s $MAILDIR/autoresponse/message.txt
 then
-  autorespond $SENDER_TIME $SENDER_NUM \
-  	$MAILDIR/autoresponse/message.txt $MAILDIR/autoresponse
+  qmail-autoresponder $MAILDIR/autoresponse/message.txt $MAILDIR/autoresponse
 fi
